4dba9a5081db5922f37a4f1589efebc1e30a5e85
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-11-06 23:51:57 -0300

    feat: adds function to see saldo

--- bank/lib/account.ex
+++ bank/lib/account.ex
@@ -4,4 +4,11 @@ defmodule Account do
   def register(user) do
     %Account{user: user}
   end
+
+  def transfer(accounts, origin, destination, value) do
+    # 10:43 https://www.youtube.com/watch?v=Y1f8G9Htivg
+  end
+
+  def saldo() do
+  end
 end
